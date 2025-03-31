ede26252109b12c80deb9620f7cbbb60b3a14053
waiyan <heinwaiyankyaw2001@gmail.com>
2025-03-31 15:45:54 +0700

    [modify] url

--- public/frontend/assets/js/app.js
+++ public/frontend/assets/js/app.js
@@ -1,3 +1,2 @@
-var BASE_API_URL = "http://localhost:8000/api/admin";
-var WEB_BASE_API_URL = "http://localhost:8000/";
-
+var BASE_API_URL = "https://www.hopehands.info/api/admin";
+var WEB_BASE_API_URL = "https://www.hopehands.info/";
