b027c4db1df3a25c1f8e7e5f9af761d208c3bd44
pyaephyo <pyaephyomg.ppm06@gmail.com>
2025-03-31 13:15:42 +0700

    remove console.log

--- resources/views/frontend/pages/auth/register.blade.php
+++ resources/views/frontend/pages/auth/register.blade.php
@@ -255,7 +255,6 @@
                     });
                 })
                 .catch(error => {
-                    console.log(error);
                     console.error('Error fetching categorys:', error);
                     categorySelect.innerHTML = `
         <option value="">ကူညီရန်အမျိုးအစားများ ရယူရာတွင် အမှားတစ်ခုဖြစ်နေပါသည်</option>
@@ -282,7 +281,6 @@
                     });
                 })
                 .catch(error => {
-                    console.log(error);
                     console.error('Error fetching divisions:', error);
                     divisionSelect.innerHTML = `
         <option value="">တိုင်းဒေသကြီးများ ရယူရာတွင် အမှားတစ်ခုဖြစ်နေပါသည်</option>
@@ -310,7 +308,6 @@
                     });
                 })
                 .catch(error => {
-                    console.log(error);
                     console.error('Error fetching divisions:', error);
                     divisionSelect.innerHTML = `
         <option value="">မြို့နယ်များ ရယူရာတွင် အမှားတစ်ခုဖြစ်နေပါသည်</option>
