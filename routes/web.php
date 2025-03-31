467691816fd01162ea0c0f2b7d4441f6a3bf390b
waiyan <heinwaiyankyaw2001@gmail.com>
2025-03-31 21:20:34 +0700

    [modify] web modify

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DonatorController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('frontend.pages.main.main'); // Load main.blade.php
+    // return view('frontend.pages.main.donators'); // Load main.blade.php
+    return redirect()->route('donators');
 });
 
 Route::get('/donator', function () {
