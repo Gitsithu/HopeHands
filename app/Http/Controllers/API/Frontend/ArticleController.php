8a6c833dd0c47eaad735197b7e455b61aec96e1f
SolomonSan11 <darthvedar11@gmail.com>
2025-04-02 18:39:00 +0700

    [add] order by in Blog

--- app/Http/Controllers/API/Frontend/ArticleController.php
+++ app/Http/Controllers/API/Frontend/ArticleController.php
@@ -20,7 +20,7 @@ class ArticleController extends Controller
 
     public function index()
     {
-        $data = Blog::orderBy('updated_at')->with('city', 'township')->paginate(9);
+        $data = Blog::orderBy('updated_at', 'desc')->with('city', 'township')->paginate(9);
         return $this->successResponse($data);
     }
 
